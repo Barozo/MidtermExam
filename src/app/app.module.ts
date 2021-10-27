9a366820e399ad253aefcf28af4077bf6558c241
Barozo <barozobenjie@gmail.com>
2021-10-28 00:44:49 +0800

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,54 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './pages/home/home.component';
+import { Routes,RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { MatCardModule } from '@angular/material/card';
+
+const routes: Routes = [
+  {
+    path: "homemanager", component: HomeComponent,
+  children: [
+    {path: "profile", component: ProfileComponent},
+    {path: "contacts", component: ContactsComponent},
+    {path: "gallery", component: GalleryComponent},
+    ]
+  },
+  {path: "**", redirectTo: "homemanager"}, 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ContactsComponent,
+    GalleryComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
